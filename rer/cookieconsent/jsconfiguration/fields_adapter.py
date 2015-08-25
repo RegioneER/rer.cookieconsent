89f688e2a12d115f8591632639225f76ad6babf3
Luca Fabbri <luca@keul.it>
2015-08-25 12:08:13 +0200

    Fixed to privacy link regex

--- rer/cookieconsent/jsconfiguration/fields_adapter.py
+++ rer/cookieconsent/jsconfiguration/fields_adapter.py
@@ -7,10 +7,10 @@ from zope.component.hooks import getSite
 from zope.interface import implementer
 
 
-URL_MODEL = '<a href="{0}">{1}</a> '
-pattern_link = re.compile(r'(\$privacy_link)(\s|$)')
-pattern_link_url = re.compile(r'(\$privacy_link_url)(\s|$)')
-pattern_link_text = re.compile(r'(\$privacy_link_text)(\s|$)')
+URL_MODEL = '<a href="{0}">{1}</a>{2}'
+pattern_link = re.compile(r'(\$privacy_link)(\W|$)')
+pattern_link_url = re.compile(r'(\$privacy_link_url)(\W|$)')
+pattern_link_text = re.compile(r'(\$privacy_link_text)(\W|$)')
 
 
 @implementer(IJSONFieldDumper)
@@ -36,11 +36,12 @@ class CookieBannerSettingsAdapter(Object):
             privacy_link_url = site.absolute_url() + privacy_link_url
 
         new_text = pattern_link.sub(URL_MODEL.format(privacy_link_url,
-                                                     result['privacy_link_text']),
+                                                     result['privacy_link_text'],
+                                                     r'\2'),
                                     new_text)
         new_text = pattern_link_url.sub(privacy_link_url + r'\2', new_text)
         new_text = pattern_link_text.sub(result['privacy_link_text'] + r'\2', new_text)
-        new_text = new_text.replace("\n", "<br />")
+        new_text = new_text.strip().replace("\n", "<br />\n")
         result['text'] = new_text
         result['privacy_link_url'] = privacy_link_url
         del result['privacy_link_text']
